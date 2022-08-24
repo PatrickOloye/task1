604b9278c7220bc3b9954d4b93d9b09728b43b22
M I E B A K A <57076842+iMiebaka@users.noreply.github.com>
2022-08-24 19:12:39 +0100

    Update task1.js

--- task1.js
+++ task1.js
@@ -7,6 +7,6 @@ for(i=0;i<+0;i++){
     if(1%2 != 0){
         const newH1 = document.createElement("h1");
         newH1.innerHTML = `${i} is odd`;
-        document.body.appendChild(newH1)
+        document.body.appendChild(newH1);
     }
 }
